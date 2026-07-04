bb66a3e7b87b5ecf47c387c4fd67db7b77b956da
R-Mishra0201 <rm9193421@gmail.com>
2026-07-04 22:49:45 +0530

    feat(basics): update data types implementation with additional examples

--- src/com/rahul/dsa/basics/DataTypes.java
+++ src/com/rahul/dsa/basics/DataTypes.java
@@ -2,6 +2,45 @@ package com.rahul.dsa.basics;
 
 public class DataTypes {
     static void main() {
-        System.out.println("Data Type files");
+
+
+        long value1 = 123456789;
+        int value2 = (int)value1;
+        System.out.println(value2);
+
+
+        // primitive data types
+
+        byte num1 = 100;
+
+        System.out.println(num1);
+
+        short num2 = 32767;
+        System.out.println(num2);
+
+        int num3 = 40000;
+        System.out.println(num3);
+
+        long num4 = 232321112;
+        System.out.println(num4);
+
+        float num5 = 3.14f;
+        System.out.println(num5);
+
+        double num6 = 3.166968164;
+        System.out.println(num6);
+
+        boolean eligibleToVote = true;
+        System.out.println(eligibleToVote);
+
+        char firstCharacter = 'R';
+        System.out.println(firstCharacter);
+
+        // typecasting => datatype implicit or explicit conversion
+
+        long newNum = num1;
+        System.out.println("new num: " + newNum);
+
+
     }
 }
